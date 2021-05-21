3441656cdcaa5ef97c7d56a3b270ddc2264e2fab
PabloMoreno2000 <pamta2000@gmail.com>
2021-05-21 15:38:56 -0500

    Added stack limit constants and renamed constants

--- consts.py
+++ consts.py
@@ -1,22 +1,20 @@
-initialAddress = 1000
-globalSize = 9000
-localSize = 10000
-temporalSize = 50000
-pointerSize = 10000
+INITIAL_ADDRESS = 1000
 
-globalFirst = initialAddress
-localFirst = globalFirst + globalSize
-temporalFirst = localFirst + localSize
-pointerFirst = temporalFirst + temporalSize
+GLOBAL_SIZE = 9000
+LOCAL_SIZE = 10000
+TEMPORAL_SIZE = 50000
+STACK_SIZE = 50000
 
+GLOBAL_FIRST = INITIAL_ADDRESS
+LOCAL_FIRST = GLOBAL_FIRST + GLOBAL_SIZE
+TEMPORAL_FIRST = LOCAL_FIRST + LOCAL_SIZE
 
 LIMITS = {
-            'GLOBAL_LIM_L' :    globalFirst,
-            'GLOBAL_LIM_R' :    globalFirst + globalSize - 1,
-            'LOCAL_LIM_L' :     localFirst,
-            'LOCAL_LIM_R' :     localFirst + localSize - 1,
-            'TEMPORAL_LIM_L' :  temporalFirst,
-            'TEMPORAL_LIM_R' :  temporalFirst + temporalSize - 1,
-            'POINTERS_LIM_L' :  pointerFirst,
-            'POINTERS_LIM_R' :  pointerFirst + pointerSize -  1
+            'GLOBAL_LIM_L' :    GLOBAL_FIRST,
+            'GLOBAL_LIM_R' :    GLOBAL_FIRST + GLOBAL_SIZE - 1,
+            'LOCAL_LIM_L' :     LOCAL_FIRST,
+            'LOCAL_LIM_R' :     LOCAL_FIRST + LOCAL_SIZE - 1,
+            'TEMPORAL_LIM_L' :  TEMPORAL_FIRST,
+            'TEMPORAL_LIM_R' :  TEMPORAL_FIRST + TEMPORAL_SIZE - 1,
+            'STACK_SIZE' :  STACK_SIZE
         }   
