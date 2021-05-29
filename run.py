003edc54f515deeb95e218caee43807f0592814e
PabloMoreno2000 <pamta2000@gmail.com>
2021-05-29 10:29:47 -0500

    Run script passes sys arguments to  compile script

--- run.py
+++ run.py
@@ -3,11 +3,13 @@ import re
 import os
 
 if __name__ == '__main__':
+
+    argumentsInString = ' '.join(sys.argv[1:])
     regex = r'(.*[\\\/])*'
     fileName = sys.argv[1]
     fileNameShort = re.sub(regex, '',fileName)
 
-    os.system('python compile.py {}'.format(fileName)) 
+    os.system('python compile.py {}'.format(argumentsInString)) 
     os.chdir('vm')
     os.system('python VENCING_VM.py ../{}'.format(fileNameShort[0 : -2] + 'o')) 
     os.chdir('..')
